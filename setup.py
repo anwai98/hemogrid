d27d3dd908bf6244f8158863b4f36758712eb0b3
anwai98 <anwai.archit@gmail.com>
2026-08-24 00:03:22 +0200

    Require a Qt backend for napari so a fresh install can open the viewers

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     url="https://github.com/anwai98/hemogrid",
     packages=find_packages(include=["hemogrid", "hemogrid.*"]),
     python_requires=">=3.11",
-    install_requires=["numpy", "scipy", "scikit-image", "tifffile", "pillow", "rich", "napari"],
+    install_requires=["numpy", "scipy", "scikit-image", "tifffile", "pillow", "rich", "napari[pyqt6]"],
     entry_points={"console_scripts": ["hemogrid=hemogrid.cli:main"]},
     classifiers=[
         "Intended Audience :: Science/Research",
